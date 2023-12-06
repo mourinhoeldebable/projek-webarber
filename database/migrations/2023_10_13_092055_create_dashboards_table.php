879eb977d7e6bc84ec53cdda7109675a8dc08257
roy <roybercos@gmail.com>
2023-12-07 04:47:43 +0700

    benerin model dashboard

--- database/migrations/2023_10_13_092055_create_dashboards_table.php
+++ database/migrations/2023_10_13_092055_create_dashboards_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\jammodel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {       
         Schema::create('dashboards', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('jammodel_id');
+            $table->foreignId('post_id');
             $table->string('pelanggan');
             $table->string('nama');
             $table->string('jam');          
